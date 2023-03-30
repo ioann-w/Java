ab3fc284b9b05e20a9618fc2f9c37bc5c0d94cbd
Ioann <i.shundeev@gmail.com>
2023-03-30 22:36:51 +0300

    create course folder

---
+++ Course/program.java
@@ -0,0 +1,48 @@
+package Course;     //class
+
+// import java.util.Scanner;
+/**
+ * program
+ */
+public class program {
+    public static void main(String[] args) {
+
+        // System.out.println("hi hi");    //sysout
+        
+        // String s = "ho ho";
+        // System.out.println(s);
+
+        // int i = 123_456;
+        // System.out.println(i);
+        // System.out.println(Integer.MAX_VALUE);
+
+        // String ss = "hoho";
+        // // ss.charAt(1);
+        // System.out.println(ss.charAt(1)); // ss.charAt(index); обращение по индексу к элементу переменной ss
+
+        // int a = 123;
+        // a = a-- - --a; // 1. a = 123, 2. a = 122, 3. a = 121, 4. 123(a1) - 121(a3)
+        // System.out.println(a);
+
+
+        // int[] arr = new int[10];    // массив
+        // System.out.println(arr.length);
+
+        // arr = new int[]{1, 2, 3, 4, 5};
+        // arr[2] = 69;
+        // System.out.println(arr[2]);;
+
+        // byte b = Byte.parseByte("1211");    // преобразование
+        // System.out.println(b);
+
+        // import java.util.Scanner; - вводится в самом начале страницы
+
+        // Scanner iScanner = new Scanner(System.in);   // Ввод с консоли
+        // System.out.printf("Ввод: ");
+        // String input = iScanner.nextLine();
+        // System.out.printf("Вы ввели: " + input);
+        // iScanner.close();
+
+    }
+    
+}
