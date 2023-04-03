425c263e4040ec750c89c447c0052601b23a4c97
Ioann <i.shundeev@gmail.com>
2023-04-03 22:57:45 +0300

    hw 2 its ok

---
+++ Homework/hw2/task2.java
@@ -0,0 +1,15 @@
+package hw2;
+
+public class task2 {
+    public static void main(String[] args) {
+        String s = "cba";
+        int[] index = {3, 2, 1};
+        StringBuilder z = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            z.setCharAt(i, s.charAt(index[i]-1));
+        }
+
+        System.out.println(z);
+    }
+}
