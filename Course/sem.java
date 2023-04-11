b61c7320d142c87981e73e3f39d20b8b60ce14b1
Ioann <i.shundeev@gmail.com>
2023-04-11 17:27:10 +0300

    sem correct

--- Course/sem.java
+++ Course/sem.java
@@ -1,61 +1,61 @@
-package Course;
+// package Course;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class sem {
-    public static void main(String[] args) {
+// public class sem {
+//     public static void main(String[] args) {
 
-        //    // Задача 1
+//         //    // Задача 1
         
-        // Scanner iScanner = new Scanner(System.in);
+//         // Scanner iScanner = new Scanner(System.in);
         
-        // System.out.printf("h: "); // вершина
-        // int h = iScanner.nextInt();
+//         // System.out.printf("h: "); // вершина
+//         // int h = iScanner.nextInt();
 
-        // System.out.printf("a: "); // пройденно за день
-        // int a = iScanner.nextInt();
+//         // System.out.printf("a: "); // пройденно за день
+//         // int a = iScanner.nextInt();
 
-        // System.out.printf("b: "); // откат за ночь
-        // int b = iScanner.nextInt();
+//         // System.out.printf("b: "); // откат за ночь
+//         // int b = iScanner.nextInt();
 
-        // int day = 1; // день первый
+//         // int day = 1; // день первый
         
-        // while (h - a > 0) {
-        //     h = h - a + b;
-        //     day++;
-        // }
+//         // while (h - a > 0) {
+//         //     h = h - a + b;
+//         //     day++;
+//         // }
 
-        // System.out.println("За " + day + " дней будет достигнута вершина");
+//         // System.out.println("За " + day + " дней будет достигнута вершина");
 
 
 
-        //     // Задача 2
+//         //     // Задача 2
 
-        // Scanner iScanner = new Scanner(System.in);
+//         // Scanner iScanner = new Scanner(System.in);
         
-        // System.out.printf("n: "); // 
-        // int n = iScanner.nextInt();
+//         // System.out.printf("n: "); // 
+//         // int n = iScanner.nextInt();
 
-        // System.out.printf("m: "); // 
-        // int m = iScanner.nextInt();
+//         // System.out.printf("m: "); // 
+//         // int m = iScanner.nextInt();
 
-        // System.out.printf("k: "); // 
-        // int k = iScanner.nextInt();
+//         // System.out.printf("k: "); // 
+//         // int k = iScanner.nextInt();
 
-        // if (k < n*m && k%n == 0 || k%m == 0)
-        // {
-        //     System.out.println("y");
-        // }
-        // else
-        // {
-        //     System.out.println("n");
-        // }
+//         // if (k < n*m && k%n == 0 || k%m == 0)
+//         // {
+//         //     System.out.println("y");
+//         // }
+//         // else
+//         // {
+//         //     System.out.println("n");
+//         // }
 
 
 
-            // Задача 3
+//             // Задача 3
 
         
-    }
+//     }
     
-}
+// }
